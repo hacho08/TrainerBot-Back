25eadfbada9a6138124f3225c0da58c38ddf584d
hacho08 <whgusdk0819@gmail.com>
2024-11-21 11:15:21 +0900

    start project

---
+++ trainerbot/src/test/java/com/smokedcorn/trainerbot/TrainerbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smokedcorn.trainerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
