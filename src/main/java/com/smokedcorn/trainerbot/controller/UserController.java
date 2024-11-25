dba4ef26aaf2c46efca55ccf29f1706a002ba7aa
wlgns98 <jihun4141@gmail.com>
2024-11-25 18:41:37 +0900

    11-25

--- src/main/java/com/smokedcorn/trainerbot/controller/UserController.java
+++ src/main/java/com/smokedcorn/trainerbot/controller/UserController.java
@@ -1,28 +1,54 @@
 package com.smokedcorn.trainerbot.controller;
 
 import com.smokedcorn.trainerbot.domain.User;
-import com.smokedcorn.trainerbot.repository.UserRepository;
+import com.smokedcorn.trainerbot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
 
-    // GET: Fetch all users
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    // POST: Add a new user
+    // GET: 사용자 조회 (ID로 조회)
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable String userId) {
+        Optional<User> user = userService.getUserById(userId);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // POST: 새로운 사용자 추가
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User savedUser = userService.addUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    // PUT: 사용자 수정
+    @PutMapping("/{userId}")
+    public ResponseEntity<User> updateUser(@PathVariable String userId, @RequestBody User user) {
+        user.setUserId(userId);  // userId가 변경되지 않도록 하기 위해 설정
+        User updatedUser = userService.updateUser(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    // DELETE: 사용자 삭제
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return ResponseEntity.noContent().build();
     }
 }
