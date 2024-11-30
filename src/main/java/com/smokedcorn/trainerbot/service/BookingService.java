3f4692e2e7fd21411f76ffc6f61b4874567e9536
wlgns98 <jihun4141@gmail.com>
2024-11-30 12:53:57 +0900

    예약 조회, 삭제

--- src/main/java/com/smokedcorn/trainerbot/service/BookingService.java
+++ src/main/java/com/smokedcorn/trainerbot/service/BookingService.java
@@ -2,48 +2,43 @@ package com.smokedcorn.trainerbot.service;
 
 import com.smokedcorn.trainerbot.domain.Booking;
 import com.smokedcorn.trainerbot.repository.BookingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookingService {
 
     private final BookingRepository bookingRepository;
 
-    @Autowired
     public BookingService(BookingRepository bookingRepository) {
+
         this.bookingRepository = bookingRepository;
     }
 
-//    // 예약 조회
-//    public Optional<Booking> getBookingById(String bookingId) {
-//        return bookingRepository.findById(bookingId);
-//    }
 
-//    // 로그인한 사용자의 예약 조회
-//    public List<Booking> getBookingById(String userId) {
-//        return bookingRepository.findByUserUserIdAndDeletedAtIsNull(userId);
-//    }
-
-
-//    // 사용자의 모든 예약 조회
-//    public List<Booking> getBookingsByUserId(String userId) {
-//        return bookingRepository.findByUserUserId(userId);
-//    }
+    // 로그인한 사용자의 예약 조회
+    public List<Booking> getBookingById(String userId) {
+        return bookingRepository.findByUserUserIdAndDeletedAtIsNull(userId);
+    }
 
     // 예약 추가
     public Booking createBooking(Booking booking) {
         return bookingRepository.save(booking);
     }
 
-    // 예약 수정
-    public Booking updateBooking(Booking booking) {
-        return bookingRepository.save(booking);
-    }
 
     // 예약 삭제
-    public void deleteBooking(String bookingId) {
-        bookingRepository.deleteById(bookingId);
+    public void deleteBooking(String userId, String bookingId) {
+        // 예약이 존재하는지 확인
+        List<Booking> booking = bookingRepository.findByUserUserIdAndDeletedAtIsNull(userId);
+
+        if (booking != null) {
+            // 예약이 존재하면 삭제된 상태로 업데이트 (deletedAt 필드 설정)
+            bookingRepository.deleteBookingByUserId(userId);
+        } else {
+            throw new RuntimeException("Reservation not found or already deleted.");
+        }
     }
 
 }
